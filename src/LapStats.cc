4e5e1324b0112c81c5afedec0487e92e0ce4e10a
Manfred Grabherr <manfred.grabherr@imbim.uu.se>
2016-04-21 13:46:10 +0200

    Added stats.

--- src/LapStats.cc
+++ src/LapStats.cc
@@ -66,7 +66,11 @@ int main( int argc, char** argv )
 
   svec<double> laps;
   laps.resize(reads.isize(), 0);
-
+  svec<double> raw_len;
+  raw_len.resize(reads.isize(), 0);
+  svec<double> lap_len;
+  lap_len.resize(reads.isize(), 0);
+ 
   FlatFileParser parser;
   
   parser.Open(fileName);
@@ -79,35 +83,62 @@ int main( int argc, char** argv )
     if (s == "Seq1")
       continue;
     int len = parser.AsInt(4);
+    int len2 = parser.AsInt(5);
 
-    // if (len > 10000)
-    //  continue;
+    //if (len < 10000)
+    //   continue;
+     
 
     int index = BinSearch(reads, s);
     if (index < 0)
       cout << "ERROR, read not found: " << s << endl;
+
+    raw_len[index] = len;
+    lap_len[index] += len2;
+ 
     laps[index] += 1.;
   }
  
   svec<double> laps_clean;
 
+  svec<double> size;
+
+  double totalLen = 0.;
+  double totalLaps = 0.;
+ 
   for (int j=0; j<laps.isize(); j++) {
     if (laps[j] > 0 && laps[j] < 200) {
       laps_clean.push_back(laps[j]);
+      size.push_back(raw_len[j]);
+      totalLaps += lap_len[j];
+      totalLen += raw_len[j];
     }
   }
+  
+  double cov = totalLaps / totalLen;
+  cout << "Total length:  " << totalLen << endl;
+  cout << "Total reads:   " << laps.isize() << endl;
+  cout << "Passing reads: " << laps_clean.isize() << endl;
+  cout << "Base coverage: " << cov << endl;
+  cout << "Genome size:   " << totalLen / cov / 1000000 <<  " MB" << endl;
+  cout << "NOTE: multiply the genome size estimate according to sub-sampling!" << endl; 
 
   Histogram hh;
 
   string dist_n = outName;
   dist_n += "/lap_dist.ps";
- 
+  string scatter_n = outName;
+  scatter_n += "/lap_scatter.ps";
+
   cout << "Plot Dist " << endl;
   hh.Plot(dist_n, laps_clean, 100, 0, 200, color(0.4, 0.2, 0.9));
 
   MakePNG(dist_n);
-  
-  
+
+  hh.Scatter(scatter_n, size, laps_clean, color(0.3, 0.0, 0.4));
+
+  MakePNG(scatter_n);
+
 
   return 0;
 }
